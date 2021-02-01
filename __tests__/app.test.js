e07d92291669f490ae92924d7c2629cb777c3ce6
Lenny Peters <lpeters3@lululemon.com>
2021-02-01 14:34:02 -0800

    refactor: 💡 moving testing folder outside of src

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,6 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import React from "react";
-import App from "../components/App";
+import App from "../src/components/App";
 
 describe("App Component", () => {
   test("show the title of the application", () => {
