fd4882ca796be3e3967d4c759e028c0b5a2a564c
Lenny Peters <lpeters3@lululemon.com>
2021-02-04 16:57:35 -0800

    refactor: 💡 updated to point to different port

--- cypress/integration/setup-example.js
+++ cypress/integration/setup-example.js
@@ -1,7 +1,7 @@
 describe("the setup-example page", () => {
   describe("adding to the count", () => {
     it("afterclicking add to the count", () => {
-      cy.visit("http://localhost:8080");
+      cy.visit("http://localhost:9000");
       cy.get("button:nth-child(2)").click();
       cy.get("p").should("have.text", "1");
       cy.get("button:nth-child(3)").click();
