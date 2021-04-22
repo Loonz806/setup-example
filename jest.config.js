fd195b52fa233fc72cfca70bb7d60ff6f2cee625
Lenny Peters <lpeters3@lululemon.com>
2021-04-22 11:04:36 -0700

    ci: 🎡 remove xml

--- jest.config.js
+++ jest.config.js
@@ -92,7 +92,7 @@ module.exports = {
   // projects: undefined,
 
   // Use this configuration option to add custom reporters to Jest
-  reporters: ["default", "jest-sonar"],
+  reporters: ["default"],
   // Automatically reset mock state between every test
   // resetMocks: false,
 
