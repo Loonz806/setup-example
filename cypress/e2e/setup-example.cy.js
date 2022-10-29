64e405412b3d127efdac2d8b2fa5c7b0977de18d
Lenny Peters <35276214+Loonz806@users.noreply.github.com>
2022-10-29 14:25:17 -0700

    feat: 🎸 updated ci, and e2e (#40)
    feat: 🎸 updated ci

--- cypress/e2e/setup-example.cy.js
+++ cypress/e2e/setup-example.cy.js
@@ -1,6 +1,26 @@
 import "cypress-axe";
 
 describe("the setup-example page", () => {
+  // Define at the top of the spec file or just import it
+  function terminalLog(violations) {
+    cy.task(
+      "log",
+      `${violations.length} accessibility violation${
+        violations.length === 1 ? "" : "s"
+      } ${violations.length === 1 ? "was" : "were"} detected`
+    );
+    // pluck specific keys to keep the table readable
+    const violationData = violations.map(
+      ({ id, impact, description, nodes }) => ({
+        id,
+        impact,
+        description,
+        nodes: nodes.length,
+      })
+    );
+
+    cy.task("table", violationData);
+  }
   // Now using Cypress/Testing-Library :)
   beforeEach(() => {
     // Load our app before starting each test case
@@ -9,9 +29,13 @@ describe("the setup-example page", () => {
   });
   it("Has no detectable a11y violations on load (filtering to only include critical impact violations)", () => {
     // Test on initial load, only report and assert for critical impact items
-    cy.checkA11y(null, {
-      includedImpacts: ["critical"],
-    });
+    cy.checkA11y(
+      null,
+      {
+        includedImpacts: ["critical"],
+      },
+      terminalLog
+    );
   });
 
   // Basic usage after interacting with the page
@@ -21,7 +45,13 @@ describe("the setup-example page", () => {
     cy.findByText(/^\+$/).should("be.visible");
     cy.findByText(/^-$/).should("be.visible");
     cy.percySnapshot("Page loaded");
-    cy.checkA11y();
+    cy.checkA11y(
+      null,
+      {
+        includedImpacts: ["critical"],
+      },
+      terminalLog
+    );
   });
 
   it("should add to the count", () => {
