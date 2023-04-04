bd796a5fcd1ff4b057fb4d6e22f170434dbb6f0b
Lenny Peters <lpeters3@lululemon.com>
2023-04-04 14:15:30 -0700

    fix: 🐛 updated
    feat: 🎸 updated
    
    chore: fix rspack build
    
    fix: 🐛 updated

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,44 @@
 const path = require("path");
-const webpack = require("webpack");
-const HtmlWebPackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+// const webpack = require("webpack");
+// const HtmlWebPackPlugin = require("html-webpack-plugin");
+// const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   mode: "development",
   devServer: {
-    contentBase: path.join(__dirname, "dist"),
-    compress: true,
+    open: true,
+    // contentBase: path.join(__dirname, "dist"),
     port: 9000,
+  },
+  builtins: {
+    html: [
+      {
+        template: "./src/index.html",
+        filename: "index.html",
+      },
+    ],
   },
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "[name].[fullhash].js",
+    filename: "[name].[contenthash].js",
   },
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-        },
+        test: /\.js$/,
+        type: "jsx",
       },
+      {
+        test: /\.ts$/,
+        type: "tsx",
+      },
+      // {
+      //   test: /\.(js|jsx)$/,
+      //   exclude: /node_modules/,
+      //   use: {
+      //     loader: "babel-loader",
+      //   },
+      // },
       // Images
       {
         test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
@@ -36,15 +52,16 @@ module.exports = {
       // CSS, PostCSS, and Sass
       {
         test: /\.(scss|css)$/,
-        use: ["style-loader", "css-loader", "postcss-loader", "sass-loader"],
+        use: ["sass-loader"],
+        type: "css",
       },
     ],
   },
   plugins: [
-    new HtmlWebPackPlugin({
-      template: "src/index.html",
-    }),
-    new webpack.HotModuleReplacementPlugin(),
-    new CleanWebpackPlugin(),
+    // new HtmlWebPackPlugin({
+    //   template: "src/index.html",
+    // }),
+    // new webpack.HotModuleReplacementPlugin(),
+    // new CleanWebpackPlugin(),
   ],
 };
